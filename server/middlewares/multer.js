bd0f42fdd09d26056c66559c1d578d8aa4028b2b
Mohd Ayan Beg <ayanbeg2409@gmail.com>
2025-09-17 14:05:57 +0530

    Update multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,11 +1,5 @@
-import multer from "multer"
-const storage=multer.diskStorage({
- destination:(req,file,cb)=>{
-    cb(null,"./public")
- },
- filename:(req,file,cb)=>{
-    cb(null,file.originalname)
- }
-})
+import multer from "multer";
 
-export const upload=multer({storage})
+const storage = multer.memoryStorage();
+
+export const upload = multer({ storage });

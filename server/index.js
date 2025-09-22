cd1518437d7483c01db3beec2e06d8f15676c128
Mohd Ayan Beg <ayanbeg2409@gmail.com>
2025-09-23 00:16:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,21 @@ app.use(express.json())
 app.use(cookieParser())
 
 
+const url = `https://render-hosting-se2b.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios.get(url).then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
